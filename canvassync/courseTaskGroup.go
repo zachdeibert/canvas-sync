93d0dc3eaf9d084d477e4a415c727e4ab215bfcf
Zach Deibert <zachdeibert@gmail.com>
2020-04-11 00:51:56 -0500

    Automatically create directories for the database

--- canvassync/courseTaskGroup.go
+++ canvassync/courseTaskGroup.go
@@ -3,17 +3,20 @@ package canvassync
 import (
 	"fmt"
 	"path"
+	"regexp"
 
 	"github.com/zachdeibert/canvas-sync/canvas"
 	"github.com/zachdeibert/canvas-sync/canvassync/coursetasks"
 	"github.com/zachdeibert/canvas-sync/task"
 )
 
+var invalidPathChars = regexp.MustCompile("[^-_a-zA-Z0-9 ()]")
+
 func courseTaskGroup(c *canvas.Canvas, db string, course courseDiscoveryResult) func(*task.Task, func()) {
 	return func(t *task.Task, finish func()) {
 		t.InheritProgress()
 		var done int = 0
-		children := coursetasks.CreateTasks(t, c, path.Join(db, fmt.Sprintf("%d - %s", course.id, course.name)), course.id)
+		children := coursetasks.CreateTasks(t, c, path.Join(db, fmt.Sprintf("%d - %s", course.id, invalidPathChars.ReplaceAllLiteralString(course.name, "_"))), course.id)
 		listener := func(_ *task.Task) {
 			if done++; done == len(children) {
 				finish()
--- canvassync/coursetasks/courseTasks.go
+++ canvassync/coursetasks/courseTasks.go
@@ -1,6 +1,7 @@
 package coursetasks
 
 import (
+	"os"
 	"path"
 
 	"github.com/zachdeibert/canvas-sync/canvas"
@@ -26,13 +27,21 @@ func register(name string, f func(*task.Task, *canvas.Canvas, string, int, func(
 	}
 }
 
+func createTask(d courseTask, c *canvas.Canvas, db string, courseID int) func(*task.Task, func()) {
+	return func(t *task.Task, finish func()) {
+		dir := path.Join(db, d.name)
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			panic(err)
+		}
+		d.f(t, c, dir, courseID, finish)
+	}
+}
+
 // CreateTasks creates all the tasks for a course under a parent task
 func CreateTasks(parent *task.Task, c *canvas.Canvas, db string, courseID int) []*task.Task {
 	res := make([]*task.Task, len(tasks))
 	for i, d := range tasks {
-		res[i] = parent.CreateSubtask(d.name, func(t *task.Task, finish func()) {
-			d.f(t, c, path.Join(db, d.name), courseID, finish)
-		})
+		res[i] = parent.CreateSubtask(d.name, createTask(d, c, db, courseID))
 	}
 	return res
 }
